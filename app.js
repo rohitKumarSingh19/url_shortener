cdfe326580c078fecda80e7ff4b26d8fe215a2a7
rohitsingh <rohit.k.chauhan1997@gmail.com>
2025-10-06 09:54:07 +0530

    create app.js , inside app foleder we created index.html and style.css inside public folder

---
+++ app.js
@@ -0,0 +1,29 @@
+import { readFile } from "fs/promises";
+import { createServer } from "http";
+import path from "path";
+const PORT = 3002;
+const serveFile = async (res,filepath,contentType) => {
+  try {
+    const data = await readFile(filepath);
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(data);
+  } catch (error) {
+    res.writeHead(404, { "Content-Type": contentType });
+    res.end("404 page not found");
+  }
+};
+const server = createServer(async (req, res) => {
+  //console.log(req.url)
+  if (req.method === "GET") {
+    if (req.url === "/") {
+      return serveFile(res,path.join("public","index.html"),"text/html");
+    } else if (req.method === "GET") {
+      if (req.url === "/style.css") {
+        return serveFile(res,path.join("public","style.css"),"text/css")
+      }
+    }
+  }
+});
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
